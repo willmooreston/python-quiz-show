33d097dfb427168501d3f295dfd4bf416870d49f
Will Mooreston <will.mooreston@gmail.com>
2026-06-25 20:21:03 -0700

    Add data structures and mixed drill to quiz registry

--- src/quizzes/index.js
+++ src/quizzes/index.js
@@ -1,8 +1,23 @@
 import * as pythonBasics from "./python-basics.js";
 import * as pythonClasses from "./python-classes.js";
+import * as dataStructures from "./data-structures.js";
 
-export const quizzes = [
+const allQuizzes = [
   { meta: pythonBasics.meta, questions: pythonBasics.questions },
   { meta: pythonClasses.meta, questions: pythonClasses.questions },
+  { meta: dataStructures.meta, questions: dataStructures.questions },
   // Add new quizzes here
 ];
+
+const mixedQuiz = {
+  meta: {
+    id: "mixed",
+    title: "Mixed Drill",
+    description: "20 random questions pulled from all quizzes",
+    topics: ["All Topics"],
+    limit: 20,
+  },
+  questions: allQuizzes.flatMap((q) => q.questions),
+};
+
+export const quizzes = [mixedQuiz, ...allQuizzes];
